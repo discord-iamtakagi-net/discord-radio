ac59cc5b649915696c2e3c77e59d6925bf98d88c
takagi <iam.takagi.dev@gmail.com>
2022-04-09 00:03:14 +0900

    `logger.py` Fix: DEBUG flag

--- app/logger.py
+++ app/logger.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-level = logging.DEBUG if os.getenv("DEBUG") else logging.INFO
+level = logging.DEBUG if os.getenv("DEBUG") == 'True' else logging.INFO
 logging.basicConfig(level=level)
 
 logger = logging.getLogger("app")
