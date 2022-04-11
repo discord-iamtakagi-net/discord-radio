2ac9a7e5e987fd1c72e1d15580122018461d353c
takagi <iam.takagi.dev@gmail.com>
2022-04-11 10:27:25 +0900

    Format

--- app/radio_stream.py
+++ app/radio_stream.py
@@ -8,8 +8,10 @@ from subprocess import PIPE, STDOUT, Popen
 from radiko import Radiko
 
 from discord import (VoiceChannel, VoiceClient)
-from discord import (VoiceChannel, VoiceClient, FFmpegPCMAudio, PCMVolumeTransformer)
+from discord import (VoiceChannel, VoiceClient,
+                     FFmpegPCMAudio, PCMVolumeTransformer)
 from discord.ext import commands
+
 
 class RadioStream():
 
@@ -22,7 +24,7 @@ class RadioStream():
 
     def __init__(
         self,
-            client: commands.Bot, 
+            client: commands.Bot,
             radiko: Radiko,
             voice_channel: VoiceChannel,
             stataion_id: str) -> None:
@@ -30,13 +32,13 @@ class RadioStream():
         self.radiko = radiko
         self.voice_channel = voice_channel
         self.station_id = stataion_id
-    
+
     def getStreamUrl(self):
         return 'http://f-radiko.smartstream.ne.jp/{}/_definst_/simul-stream.stream/playlist.m3u8'.format(self.station_id)
-    
+
     async def connect(self):
         self.voice_client = await self.voice_channel.connect()
-    
+
     async def disconnect(self):
         await self.voice_client.disconnect()
         self.voice_client = None
@@ -48,14 +50,17 @@ class RadioStream():
     async def playRadioStream(self):
         while self.client.loop.is_running():
 
-            url = self.radiko.gen_temp_chunk_m3u8_url(self.getStreamUrl(), Radiko.token)
-            p =  Popen("ffmpeg -y -vn -headers 'X-Radiko-AuthToken: {}' -i '{}' -acodec copy -f adts -loglevel error /dev/stdout".format(Radiko.token, url), 
-                shell=True, stdout=PIPE, stderr=STDOUT, preexec_fn=os.setsid)
-            source = PCMVolumeTransformer(FFmpegPCMAudio(p.stdout, pipe=True), volume=1.0)
-            
+            url = self.radiko.gen_temp_chunk_m3u8_url(
+                self.getStreamUrl(), Radiko.token)
+            p = Popen("ffmpeg -y -vn -headers 'X-Radiko-AuthToken: {}' -i '{}' -acodec copy -f adts -loglevel error /dev/stdout".format(Radiko.token, url),
+                      shell=True, stdout=PIPE, stderr=STDOUT, preexec_fn=os.setsid)
+            source = PCMVolumeTransformer(
+                FFmpegPCMAudio(p.stdout, pipe=True), volume=1.0)
+
             if self.voice_client and not self.voice_client.is_playing():
                 self.voice_client.play(source, after=lambda _: p.kill())
-            
-            logger.debug('started subprocess: group id {}'.format(os.getpgid(p.pid)))
 
-            await asyncio.sleep(10) # 10秒起きにループ
+            logger.debug(
+                'started subprocess: group id {}'.format(os.getpgid(p.pid)))
+
+            await asyncio.sleep(10)  # 10秒起きにループ
